2112e86b614118163a81774346d8d5b2d7447fd9
sk0g <tenderanger@gmail.com>
2017-02-03 15:04:19 +1000

    teamPicker now returns, fixed typo
    Teampicker now returns teamname, instead of just printing it out to the
    console
    "Alligators" instead of "Aligators"

--- teamPicker.py
+++ teamPicker.py
@@ -1,9 +1,10 @@
 import random
 
-teams = ['Tirana', 'Vienna', 'Brussels', 'Sofia', 'Prague', 'Tallinn', 'Berlin', 'Helsinki', 'Athens', 'Budapest', 'Dublin', 'Rome', ' Kosovo', 'Vaduz', 'Luxembourg', 'Valletta', 'Amsterdam', 'Monaco', 'Warsaw', 'Bucharest', 'San Marino', 'Bratislava', 'Madrid', 'Berne', 'London', 'Vatican City', 'Kiev', 'Stockholm', 'Ljubljana', 'Belgrade', 'Moscow', 'Oslo', 'Podgorica', 'Chisinau', 'Skopje', 'Vilnius', 'Riga', 'Saint Heller', 'Douglas', 'Reykjavik', 'Gibraltar', 'Paris', 'Tórshavn', 'Copenhagen', 'Zagreb', 'Sarajevo', 'Minsk', 'Tbilisi', 'Cardiff']
+def teamPicker():
+    teams = ['Tirana', 'Vienna', 'Brussels', 'Sofia', 'Prague', 'Tallinn', 'Berlin', 'Helsinki', 'Athens', 'Budapest', 'Dublin', 'Rome', ' Kosovo', 'Vaduz', 'Luxembourg', 'Valletta', 'Amsterdam', 'Monaco', 'Warsaw', 'Bucharest', 'San Marino', 'Bratislava', 'Madrid', 'Berne', 'London', 'Vatican City', 'Kiev', 'Stockholm', 'Ljubljana', 'Belgrade', 'Moscow', 'Oslo', 'Podgorica', 'Chisinau', 'Skopje', 'Vilnius', 'Riga', 'Saint Heller', 'Douglas', 'Reykjavik', 'Gibraltar', 'Paris', 'Tórshavn', 'Copenhagen', 'Zagreb', 'Sarajevo', 'Minsk', 'Tbilisi', 'Cardiff']
 
-animals = ['Dragons', 'Lions', 'Cats', 'Lynxes', 'Tigers', 'Sharks', 'Jaguars', 'Panthers', 'Cobras', 'Bisons', 'Aligators', 'Scorpions', 'Honey-Badgers', 'Piranhas', 'Porcupines', 'Warthogs', 'Wolves', 'Zebras']
+    animals = ['Dragons', 'Lions', 'Cats', 'Lynxes', 'Tigers', 'Sharks', 'Jaguars', 'Panthers', 'Cobras', 'Bisons', 'Alligators', 'Scorpions', 'Honey-Badgers', 'Piranhas', 'Porcupines', 'Warthogs', 'Wolves', 'Zebras']
 
-teamname = random.choice(teams) + ' ' + random.choice(animals)
+    teamname = random.choice(teams) + ' ' + random.choice(animals)
 
-print(teamname)
+    return teamname
