b5cd06e19282d526e6da504bd9f0e9055f0ad54f
Daddyhughes111 <dathughes11@gmail.com>
2017-02-07 21:36:54 +0000

    Added more animals for more variety

--- teamPicker.py
+++ teamPicker.py
@@ -3,7 +3,7 @@ import random
 def teamPicker():
     teams = ['Tirana', 'Vienna', 'Brussels', 'Sofia', 'Prague', 'Tallinn', 'Berlin', 'Helsinki', 'Athens', 'Budapest', 'Dublin', 'Rome', ' Kosovo', 'Vaduz', 'Luxembourg', 'Valletta', 'Amsterdam', 'Monaco', 'Warsaw', 'Bucharest', 'San Marino', 'Bratislava', 'Madrid', 'Berne', 'London', 'Vatican City', 'Kiev', 'Stockholm', 'Ljubljana', 'Belgrade', 'Moscow', 'Oslo', 'Podgorica', 'Chisinau', 'Skopje', 'Vilnius', 'Riga', 'Saint Heller', 'Douglas', 'Reykjavik', 'Gibraltar', 'Paris', 'Tórshavn', 'Copenhagen', 'Zagreb', 'Sarajevo', 'Minsk', 'Tbilisi', 'Cardiff']
 
-    animals = ['Dragons', 'Lions', 'Cats', 'Lynxes', 'Tigers', 'Sharks', 'Jaguars', 'Panthers', 'Cobras', 'Bisons', 'Alligators', 'Scorpions', 'Honey-Badgers', 'Piranhas', 'Porcupines', 'Warthogs', 'Wolves', 'Zebras']
+    animals = ['Dragons', 'Lions', 'Cats', 'Lynxes', 'Tigers', 'Sharks', 'Jaguars', 'Panthers', 'Cobras', 'Bisons', 'Alligators', 'Scorpions', 'Honey-Badgers', 'Piranhas', 'Porcupines', 'Warthogs', 'Wolves', 'Zebras', 'Spiders', 'Vipers', 'Eagles', 'Gorillas']
 
     teamname = random.choice(teams) + ' ' + random.choice(animals)
 
