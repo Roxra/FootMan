e29d05650661e9719450ef438a8ba99ffeb75480
William Hughes <dathughes11@gmail.com>
2017-02-08 14:28:57 +0000

    Updated namepicker.py and started working on teampicker.py (#3)
    * Generates random name
    
    * fixed spacing
    
    * teamPicker.py updated
    
    categorised some of the capital cities into which nationality they are from.
    added random team names depending on the nationality and worldwide
    
    * not working yet
    
    * Update teamPicker.py
    
    * Update teamPicker.py

---
+++ namePicker.py
@@ -0,0 +1,29 @@
+import random
+
+from teamPicker.py import teamPicker()
+
+def namepicker():
+  if teamPicker(teams) == 'Balkans': 
+    first_name = ['
+    second_name = ['
+  elif teamPicker(teams) == 'Britain'
+  
+
+balkanFirstNames = ['
+balkanLastNames = ['
+                   
+britishFirstNames = ['David', 'William', 'Robert', 'Daniel', 'John', 'James', 'Micheal', 'Paul', 'Mark']
+britishLastNames = ['Smith', 'Johnson', 'Williams', 'Browns', 'Wilson', 'Moore', 'Anderson']
+
+               
+#none of this code works right now still working on it but i think we need to fix the team picker file first               
+               
+              
+#def namePicker(): I figure most of this is probably pointless now
+  #first_name = ['David', 'William', 'Robert', 'Daniel', 'John', 'James', 'Micheal', 'Paul', 'Mark']
+  
+  #second_name = ['Smith', 'Johnson', 'Williams', 'Browns', 'Wilson', 'Moore', 'Anderson']
+  
+  #fullname = random.choice(first_name) + " " + random.choice(second_name)
+  
+  #return fullname
--- teamPicker.py
+++ teamPicker.py
@@ -1,10 +1,24 @@
 import random
 
 def teamPicker():
-    teams = ['Tirana', 'Vienna', 'Brussels', 'Sofia', 'Prague', 'Tallinn', 'Berlin', 'Helsinki', 'Athens', 'Budapest', 'Dublin', 'Rome', ' Kosovo', 'Vaduz', 'Luxembourg', 'Valletta', 'Amsterdam', 'Monaco', 'Warsaw', 'Bucharest', 'San Marino', 'Bratislava', 'Madrid', 'Berne', 'London', 'Vatican City', 'Kiev', 'Stockholm', 'Ljubljana', 'Belgrade', 'Moscow', 'Oslo', 'Podgorica', 'Chisinau', 'Skopje', 'Vilnius', 'Riga', 'Saint Heller', 'Douglas', 'Reykjavik', 'Gibraltar', 'Paris', 'Tórshavn', 'Copenhagen', 'Zagreb', 'Sarajevo', 'Minsk', 'Tbilisi', 'Cardiff']
 
+    #Teams = ['Tirana', 'Vienna', 'Brussels', 'Sofia', 'Prague', 'Tallinn', 'Berlin', 'Helsinki', 'Athens', 'Budapest', 'Dublin', 'Rome', ' Kosovo', 'Vaduz', 'Luxembourg', 'Valletta', 'Amsterdam', 'Monaco', 'Warsaw', 'Bucharest', 'San Marino', 'Bratislava', 'Madrid', 'Berne', 'London', 'Vatican City', 'Kiev', 'Stockholm', 'Ljubljana', 'Belgrade', 'Moscow', 'Oslo', 'Podgorica', 'Chisinau', 'Skopje', 'Vilnius', 'Riga', 'Saint Heller', 'Douglas', 'Reykjavik', 'Gibraltar', 'Paris', 'Tórshavn', 'Copenhagen', 'Zagreb', 'Sarajevo', 'Minsk', 'Tbilisi', 'Cardiff']
+    
+    britain = ['London', 'Cardiff', 'Dublin', 'Edinburgh']
+    
+    balkans = ['Tirana', 'Sofia', 'Belgrade', 'Zagreb', 'Ljubljana', 'Sarajevo', 'Rome', 'Bucharest', 'Tirana', 'Ankara', 'Athens']
+    
+    scandinavia = ['Copenhagen', 'Oslo', 'Stockholm', 'Helsinki', 'Reykjavík'] #I have not added all the ones you origanlly wrote in teams yet
+    
+    germanic = ['Berlin', 'Vienna']
+    
     animals = ['Dragons', 'Lions', 'Cats', 'Lynxes', 'Tigers', 'Sharks', 'Jaguars', 'Panthers', 'Cobras', 'Bisons', 'Alligators', 'Scorpions', 'Honey-Badgers', 'Piranhas', 'Porcupines', 'Warthogs', 'Wolves', 'Zebras', 'Spiders', 'Vipers', 'Eagles', 'Gorillas']
 
-    teamname = random.choice(teams) + ' ' + random.choice(animals)
-
-    return teamname
+    britainteamname = random.choice(britain) + ' ' + random.choice(animals)
+    balkansteamname = random.choice(balkans) + ' ' + random.choice(animals)
+    scandinaviateamname = random.choice(scandinavia) + ' ' + random.choice(animals)
+    worldwideteamname = random.choice(britain+balkans+scandinavia) + ' ' + random.choice(animals)
+                   
+    return worldwideteamname
+                   
+    #i guess we need some sort of input for what nationality you want? or is it just going to be random and the nationality is left "behind the scenes"?
