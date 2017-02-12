1e6dbd6f5637af9802b3887b1e00dae323cf065c
sk0g <tenderanger@gmail.com>
2017-02-12 12:35:31 +1000

    Fixed region selection logic Fix return logic Clean up code

--- teamGen.py
+++ teamGen.py
@@ -1,12 +1,19 @@
 import random
 
+CITIES_BRITAIN = ['London', 'Cardiff', 'Dublin', 'Edinburgh', 'Manchester', 'Southampton', 'Liverpool', 'Bristol', 'Leeds', 'Cambridge', 'Glasgow']
+CITIES_SCANDINAVIA = ['Copenhagen', 'Aarhus', 'Oslo' 'Bergen', 'Trondheim', 'Gothenburg', 'Malmö', 'Stockholm', 'Helsinki', 'Tampere', 'Jyväskylä', 'Reykjavík', 'Tórshavn']
+CITIES_GERMAN = ['Berlin', 'Munich', 'Hamburg', 'Dortmund', 'Essen', 'Hanover', 'Bremen', 'Düsseldorf' 'Vienna', 'Salzburg', 'Graz', 'Bern', 'Zürich', 'Basel']
+
 def team_picker():
     animals = ['Dragons', 'Lions', 'Cats', 'Lynxes', 'Tigers', 'Sharks', 'Jaguars', 'Panthers', 'Cobras', 'Bisons', 'Alligators', 'Scorpions', 'Honey-Badgers', 'Piranhas', 'Porcupines', 'Warthogs', 'Wolves', 'Zebras', 'Spiders', 'Vipers', 'Eagles', 'Gorillas']
 
     regions = ['britain', 'scandinavia', 'german']
-    selected = random.choice(regions)
-    teamname = random.choice(selected) + ' ' + random.choice(animals)
+    city = random.choice(regions)
+    if city == 'britain':
+        city = random.choice(CITIES_BRITAIN)
+    elif city == 'scandinavia':
+        city = random.choice(CITIES_SCANDINAVIA)
+    else:
+        city = random.choice(CITIES_GERMAN)
 
-CITIES_BRITISH = ['London', 'Cardiff', 'Dublin', 'Edinburgh', 'Manchester', 'Southampton', 'Liverpool', 'Bristol', 'Leeds', 'Cambridge', 'Glasgow']
-CITIES_SCANDINAVIAN = ['Copenhagen', 'Aarhus', 'Oslo' 'Bergen', 'Trondheim', 'Gothenburg', 'Malmö', 'Stockholm', 'Helsinki', 'Tampere', 'Jyväskylä', 'Reykjavík', 'Tórshavn']
-CITIES_GERMAN = ['Berlin', 'Munich', 'Hamburg', 'Dortmund', 'Essen', 'Hanover', 'Bremen', 'Düsseldorf' 'Vienna', 'Salzburg', 'Graz', 'Bern', 'Zürich', 'Basel']
+    teamname = city + ' ' + random.choice(animals)
