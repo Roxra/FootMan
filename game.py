8e17369be95e64134cef2809149646f0f251d430
sk0g <tenderanger@gmail.com>
2017-02-13 21:04:25 +1000

    Further improvements on playerGen
    Now uses global variables for height, weight, stats
    Added in basic version of player_stats_gen

--- game.py
+++ game.py
@@ -1,17 +1,22 @@
 from teamGen import *
+from playerGen import *
 
 class club:
     def __init__(self):
         self.name = ''
         self.city = ''
         self.mascot = ''
+        self.players = []
 
 # Initialise clubs
 team1 = club()
 team2 = club()
 
-# Send clubs to team_picker to add city and teamname
+# Send clubs to team_picker to add city and team name
 team_picker(team1)
 team_picker(team2)
 
 print("For today's match, we have " + team1.name + " facing " + team2.name)
+
+# for i in range (0, 40):
+#     team1.players.append()
--- playerGen.py
+++ playerGen.py
@@ -1,15 +1,24 @@
 import random
+import numpy as np
 from teamGen import *
 from lists import *
+
+DEFAULT_HEIGHT = 180
+VARIANCE_HEIGHT = 5
+DEFAULT_WEIGHT = 75
+VARIANCE_WEIGHT = 7
+DEFAULT_STAT = 19
+VARIANCE_STAT = 6
 
 class player:
     def __init__(self, name):
         self.name = name
         self.city = 'DefCity'
-        self.height = 175
-        self.strength = 20.0
-        self.defence = 20.0
-        self.aggression = 20.0
+        self.height = DEFAULT_HEIGHT
+        self.weight = DEFAULT_WEIGHT
+        self.strength = DEFAULT_WEIGHT
+        self.defence = DEFAULT_STAT
+        self.aggression = DEFAULT_STAT
 
 def player_name_gen(club, p):
     if club.city in CITIES_BRITAIN:
@@ -18,6 +27,11 @@ def player_name_gen(club, p):
         p.name = two_list_rand(nordic_first, nordic_last)
     elif club.city in CITIES_GERMAN:
         p.name = two_list_rand(german_first, german_last)
+
+def player_stats_gen(p):
+    p.strength = np.random.normal(DEFAULT_STAT, VARIANCE_STAT)
+
+    p.height = round(np.random.normal(DEFAULT_HEIGHT, VARIANCE_HEIGHT))
 
 def two_list_rand(first, last):
     return random.choice(first) + ' ' + random.choice(last)
