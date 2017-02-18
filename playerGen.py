d3cc407128d42022134d68acfc591a4748939df0
sk0g <tenderanger@gmail.com>
2017-02-19 07:01:07 +1000

    Update global variable names to be easier to read

--- playerGen.py
+++ playerGen.py
@@ -3,22 +3,22 @@ import numpy as np
 from teamGen import *
 from lists import *
 
-DEFAULT_HEIGHT = 180
-VARIANCE_HEIGHT = 5
-DEFAULT_WEIGHT = 75
-VARIANCE_WEIGHT = 7
-DEFAULT_STAT = 19
-VARIANCE_STAT = 6
+HEIGHT_DEFAULT = 180
+HEIGHT_VARIANCE = 5
+BMI_DEFAULT = 75
+BMI_VARIANCE = 7
+STAT_DEFAULT = 19
+STAT_VARIANCE = 6
 
 class player:
     def __init__(self, name):
         self.name = name
         self.city = 'DefCity'
-        self.height = DEFAULT_HEIGHT
+        self.height = HEIGHT_DEFAULT
         self.weight = DEFAULT_WEIGHT
         self.strength = DEFAULT_WEIGHT
-        self.defence = DEFAULT_STAT
-        self.aggression = DEFAULT_STAT
+        self.defence = STAT_DEFAULT
+        self.aggression = STAT_DEFAULT
 
 def player_name_gen(club, p):
     if club.city in CITIES_BRITAIN:
@@ -29,9 +29,9 @@ def player_name_gen(club, p):
         p.name = two_list_rand(german_first, german_last)
 
 def player_stats_gen(p):
-    p.strength = np.random.normal(DEFAULT_STAT, VARIANCE_STAT)
-
-    p.height = round(np.random.normal(DEFAULT_HEIGHT, VARIANCE_HEIGHT))
+    p.strength = np.random.normal(STAT_DEFAULT, STAT_VARIANCE)
+    p.weight = player_weight_gen(p)
+    p.height = round(np.random.normal(HEIGHT_DEFAULT, HEIGHT_VARIANCE))
 
 def two_list_rand(first, last):
     return random.choice(first) + ' ' + random.choice(last)
