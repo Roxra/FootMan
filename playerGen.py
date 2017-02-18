4e8dea0a2e07288da6f869eb9bf65b4379a1537d
sk0g <tenderanger@gmail.com>
2017-02-19 07:16:34 +1000

    use BMI formula to generate a realistic weight

--- playerGen.py
+++ playerGen.py
@@ -5,20 +5,21 @@ from lists import *
 
 HEIGHT_DEFAULT = 180
 HEIGHT_VARIANCE = 5
-BMI_DEFAULT = 75
-BMI_VARIANCE = 7
+BMI_DEFAULT = 25
+BMI_VARIANCE = 2
 STAT_DEFAULT = 19
 STAT_VARIANCE = 6
+STAT_INIT = 0
 
 class player:
     def __init__(self, name):
         self.name = name
         self.city = 'DefCity'
-        self.height = HEIGHT_DEFAULT
-        self.weight = DEFAULT_WEIGHT
-        self.strength = DEFAULT_WEIGHT
-        self.defence = STAT_DEFAULT
-        self.aggression = STAT_DEFAULT
+        self.height = STAT_INIT
+        self.weight = STAT_INIT
+        self.strength = STAT_INIT
+        self.defence = STAT_INIT
+        self.aggression = STAT_INIT
 
 def player_name_gen(club, p):
     if club.city in CITIES_BRITAIN:
@@ -30,8 +31,16 @@ def player_name_gen(club, p):
 
 def player_stats_gen(p):
     p.strength = np.random.normal(STAT_DEFAULT, STAT_VARIANCE)
-    p.weight = player_weight_gen(p)
     p.height = round(np.random.normal(HEIGHT_DEFAULT, HEIGHT_VARIANCE))
+    p.weight = player_weight_gen(p)
 
 def two_list_rand(first, last):
     return random.choice(first) + ' ' + random.choice(last)
+
+def player_weight_gen(p):
+    # Generate random BMI
+    bmi = np.random.normal(BMI_DEFAULT, BMI_VARIANCE)
+
+    # Convert weight to M for calculation, and use BMI converting formula
+    weight = bmi * (p.height / 100) ** 2
+    return weight
